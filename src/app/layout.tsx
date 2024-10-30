470cd54f5ce09330fb4497df921bccac5a31ddcd
kauameloo <kaua.rodrigues2620@gmail.com>
2024-10-30 00:22:03 -0300

    layout.tsx atualizada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,36 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "../components/theme-provider/theme-provider";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Chatbot from "@/components/Chatbot/Chatbot";
+// import Chatbot from './components/Chatbot'
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AutoRecomenda",
+  description: "Sistema de recomendação de oficinas automotivas",
+};
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-      >
-        {children}
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow container mx-auto px-4 py-8">
+              {children}
+            </main>
+            <Footer />
+          </div>
+          <Chatbot />
+        </ThemeProvider>
       </body>
     </html>
   );
